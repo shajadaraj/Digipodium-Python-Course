9f329302ebbe2f9d69929e1c7e69bf3d334d3cb7
shajadaraj <shajadaraj2512@gmail.com>
2023-02-07 19:52:15 +0530

    design

---
+++ t1.py
@@ -0,0 +1,18 @@
+# turtle is a graphic library
+from turtle import *
+
+speed('slowest')
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(240)
+left(90)
+forward(120)
+left(90)
+forward(120)
+
+mainloop()
---
+++ t2.py
@@ -0,0 +1,13 @@
+from turtle import *
+speed('slowest')
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+mainloop()
